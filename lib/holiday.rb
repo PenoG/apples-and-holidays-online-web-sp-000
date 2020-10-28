73399c38c19fd95f890234db8fcfaaa0566b18d1
Mason Phillips <mjphillips2000@gmail.com>
2020-10-28 05:08:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,6 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
 
   holiday_hash[:winter][:christmas] << "Balloons"
+  holiday_hash[:winter][:new_yearss] << "Balloons"
   
 end
 
